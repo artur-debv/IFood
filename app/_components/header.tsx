61c785f940644546918fb7542f1bcca50a024301
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-05-05 01:53:40 -0300

    update

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
               className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
             >
               <HomeIcon size={16} />
-             <Link href="/layout">
+             <Link href="/fullstackweek-foods-main/app/page">
              <span className="block" >Início</span>
              </Link>
             </Button>
