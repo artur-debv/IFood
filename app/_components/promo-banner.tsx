7ca75ae3e4b98e1332f845bec9e4dbca2403ad5f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-05-11 23:06:53 -0300

    Update PPromo-banner.tsx

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -3,8 +3,8 @@ import Image, { ImageProps } from "next/image";
 const PromoBanner = (props: ImageProps) => {
   return (
     <Image
-    height={500} // Defina a altura desejada da imagem aqui
-    width={500}  // Defina a largura desejada da imagem aqui
+      height={0} // Defina a altura desejada da imagem aqui
+      width={0}  // Defina a largura desejada da imagem aqui
       className="h-auto w-full object-contain lg:h-80 "
       sizes="100vw"
       quality={100}
